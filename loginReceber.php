74c571d98ec7a244ef80d36f0a5f263e0518ecaa
unknown <franciscomarcio01@gmail.com>
2018-05-25 16:00:10 -0300

    telas

--- loginReceber.php
+++ loginReceber.php
@@ -1,13 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
 <?php 
 
 	$login = $_POST['login'];
 	$senha = $_POST['senha'];
 	
 	include'banco.php';
-	$query = $db->query("SELECT login FROM login where login = $_POST['login']");
 
-	$linha = $query->fetch();
-	$linha['login'];
-	echo $linha
-
+	function buscar_tarefas($conexao)
+	{
+		$sqlBusca = 'SELECT * FROM login where login = "marcio"';
+		$resultado = mysqli_query($conexao, $sqlBusca);
+		$tarefas = array();
+			while ($tarefa = mysqli_fetch_assoc($resultado)) {
+				$tarefas[] = $tarefa;
+			}
+		return $tarefas;
+	}
+	$a = buscar_tarefas($conexao);
+	$b ="";
 ?>
+<a href="teste.php"></a>
+</body>
+</html>
