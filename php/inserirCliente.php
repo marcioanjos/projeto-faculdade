c347c25ecfbaef529b756ec9617a1c844ba70da2
unknown <franciscomarcio01@gmail.com>
2018-06-01 16:12:27 -0300

    ajustes simples

--- php/inserirCliente.php
+++ php/inserirCliente.php
@@ -5,12 +5,16 @@
     //verificar qual o id do quarto esta sendo resevando
     if(isset($_POST['01'])){
         $idQuarto = 1;
+        $tipoQuarto = 'casal';
         }else if (isset($_POST['02'])) {
             $idQuarto = 2;
+             $tipoQuarto = 'solteiro';
             }else if(isset($_POST['03'])){
                 $idQuarto = 3;
+                 $tipoQuarto = 'frente ao mar';
                 }else if (isset($_POST['0'])) {
                     $idQuarto = 4;
+                     $tipoQuarto = 'familia';
     }
     //verificar o sexo selecionado
     if (isset($_POST['05'])) {
@@ -31,6 +35,8 @@
     $nacionalidade = $_POST['NACIONALIDADE'];
     $email = $_POST['E-MAIL'];
     $estadocivil = $_POST['ESTADOCIVIL'];
+    $dataInicio = $_POST['DATAINICIO'];
+    $dataFim = $_POST['DATAFIM'];
 
     //inserer na tabela clientes
     $linhasInseridas = $db->exec("INSERT INTO cliente (cpf, rg, nome, enderenco, numero, complemeto, bairro, cidade, cep, dataNascimento, nacionalidade, sexo, estadoCivil, email)
@@ -40,6 +46,6 @@
     if ($linhasInseridas) {
 		header("location: ../html/cliente.html");
 	   } else {
-		  echo 'Não foi possível inserir';
+		  echo 'Não foi possível inserir';    
 	}
-    ?>
+?>
